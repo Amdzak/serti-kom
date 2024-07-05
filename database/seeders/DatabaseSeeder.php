2cef18752dd2f880a92abc2e568e1a94a50d555d
Ahmad Zulfikar Kurniawan <ahmadzulfikar133@gmail.com>
2024-07-05 22:33:15 +0700

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        DB::table('arsip_surat')->insert(
+            [
+                'nomor_surat' => '123/123/123',
+                'judul' => "hehe",
+                'id_kategori' => 1,
+            ],
+            [
+                'nomor_surat' => '123/123/123',
+                'judul' => "hehe",
+                'id_kategori' => 2,
+            ],
+            [
+                'nomor_surat' => '123/123/123',
+                'judul' => "hehe",
+                'id_kategori' => 3,
+            ],
+        );
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        DB::table('kategori_surat')->insert(
+            [
+                'nama_kategori' => 'Pembangunan Jalan',
+                'keterangan' => "pembangunan jalan di daerah masing masing",
+            ],
+            [
+                'nama_kategori' => 'Renovasi Jalan',
+                'keterangan' => "pembangunan jalan di daerah masing masing",
+            ],
+        );
     }
 }
